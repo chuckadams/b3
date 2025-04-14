a3ba338a88dbcde2f210692ca4652c99ff504fd2
Chuck Adams <cja987@gmail.com>
2025-04-14 16:19:04 -0600

    refactor: fix all warnings in b2edit.form.php

--- public/b2-include/b2edit.form.php
+++ public/b2-include/b2edit.form.php
@@ -1,5 +1,4 @@
-<?php
-echo $tabletop;
+<?= $tabletop;
 
 $action ??= $_POST['action'] ?? $_GET['action'] ?? '';
 $user_ID ??= 69696969696969; // silly value but it should already be set
@@ -34,68 +33,64 @@ switch ($action) {
 ?>
 
 <form name="post" action="b2edit.php" method="POST">
-  <input type="hidden" name="user_ID" value="<?php echo $user_ID ?>"/>
-  <input type="hidden" name="action" value="<?php echo $form_action . $form_extra ?>"/>
+  <input type="hidden" name="user_ID" value="<?= $user_ID ?>"/>
+  <input type="hidden" name="action" value="<?= $form_action . $form_extra ?>"/>
 
   <table cellspacing="0" cellpadding="0" border="0" width="100%">
     <tr>
-      <td<?php
-
-      if ($action != "editcomment") {
-      // this is for everything but comment editing
-      ?>>
-        <table height="60" align="left" cellpadding="0" cellspacing="0">
-          <td height="60" width="190">
-            <label for="title"><b>Title :</b></label><br/>
-            <input type="text" name="post_title" size="20" tabindex="1" style="width: 170px;" value="<?php echo $edited_post_title; ?>" id="title"/>
-          </td>
-          <td>
-            <label for="category"><b>Category :</b></label><br/><?php dropdown_categories(); ?>
-          </td>
+        <?php if ($action !== "editcomment") { ?>
+      <td>
+        <table align="left" cellpadding="0" cellspacing="0">
+          <tr>
+            <td height="60" width="190">
+              <label for="title"><b>Title :</b></label><br/>
+              <input type="text" name="post_title" size="20" tabindex="1" style="width: 170px;" value="<?= $edited_post_title ?>" id="title"/>
+            </td>
+            <td>
+              <label for="category"><b>Category :</b></label><br/><?php dropdown_categories(); ?>
+            </td>
+          </tr>
         </table>
-          <?php
-          } else {
-          // this is for comment editing
-          ?> colspan="2">&nbsp;
+          <?php } else { ?>
+      <td colspan="2">&nbsp;
       </td>
     </tr>
 
     <tr>
       <td>
         <label for="name"><b>Name :</b></label><br/>
-        <input type="text" name="newcomment_author" size="20" value="<?php echo format_to_edit($commentdata["comment_author"]) ?>" tabindex="1" id="name"/></td>
+        <input type="text" name="newcomment_author" size="20" value="<?= format_to_edit($commentdata["comment_author"]) ?>" tabindex="1" id="name"/></td>
       <td>
         <label for="email"><b>E-mail :</b></label><br/>
-        <input type="text" name="newcomment_author_email" size="20" value="<?php echo format_to_edit($commentdata["comment_author_email"]) ?>" tabindex="2" id="email"/></td>
+        <input type="text" name="newcomment_author_email" size="20" value="<?= format_to_edit($commentdata["comment_author_email"]) ?>" tabindex="2" id="email"/></td>
       <td>
         <label for="URL"><b>URL :</b></label><br/>
-        <input type="text" name="newcomment_author_url" size="20" value="<?php echo format_to_edit($commentdata["comment_author_url"]) ?>" tabindex="3" id="URL"/>
-          <?php
-          }
-
-          ?>
+        <input type="text" name="newcomment_author_url" size="20" value="<?= format_to_edit($commentdata["comment_author_url"]) ?>" tabindex="3" id="URL"/>
+          <?php } ?>
       </td>
     </tr>
     <tr>
-      <td colspan="<?php echo $colspan; ?>">
+      <td colspan="<?= $colspan ?>">
         <table cellspacing="0" cellpadding="0" border="0" width="100%">
-          <td valign="bottom">
-              <?php
-              if ($action != 'editcomment') {
-                  echo '<label for="content"><b>Post :</b></label>';
-              } else {
-                  echo '<br /><label for="content"><b>Comment :</b></label>';
-              }
-              ?>
-          </td>
-          <td valign="bottom" align="right">
-              <?php if ($use_quicktags) {
-                  include($b2inc . '/b2quicktags.php');
-              } ?>
-          </td>
+          <tr>
+            <td valign="bottom">
+                <?php
+                if ($action !== 'editcomment') {
+                    echo '<label for="content"><b>Post :</b></label>';
+                } else {
+                    echo '<br /><label for="content"><b>Comment :</b></label>';
+                }
+                ?>
+            </td>
+            <td valign="bottom" align="right">
+                <?php if ($use_quicktags) {
+                    include($b2inc . '/b2quicktags.php');
+                } ?>
+            </td>
+          </tr>
         </table>
 
-        <textarea rows="9" cols="40" style="width:100%" name="content" tabindex="4" wrap="virtual" id="content"><?php echo $content ?></textarea><br/>
+        <textarea rows="9" cols="40" style="width:100%" name="content" tabindex="4" id="content"><?= $content ?></textarea><br/>
 
         <input
             type="checkbox" class="checkbox" name="post_autobr" value="1" <?php
@@ -107,29 +102,22 @@ switch ($action) {
             <input type="button" value="preview" onclick="preview(this.form);" class="search" tabindex="8"/>
           <?php } ?>
 
-        <input type="submit" name="submit" value="<?php echo $submitbutton_text ?>" class="search" style="font-weight: bold;" tabindex="5"/>
+        <input type="submit" name="submit" value="<?= $submitbutton_text ?>" class="search" style="font-weight: bold;" tabindex="5"/>
 
           <?php if (($use_fileupload) && ($user_level >= $fileupload_minlevel)
               && ((str_contains($fileupload_allowedusers, " " . $user_login . " "))
-                  || (trim(
-                          $fileupload_allowedusers,
-                      ) == ""))) { ?>
+                  || (trim($fileupload_allowedusers) === ""))) { ?>
             <input type="button" value="upload a file/image" onclick="launchupload();" class="search" tabindex="10"/>
           <?php }
 
           // if the level is 5+, allow user to edit the timestamp - not on 'new post' screen though
           #if (($user_level > 4) && ($action != "post"))
           if ($user_level > 4) {
-              touch_time(($action == "edit"));
+              touch_time(($action === "edit"));
           }
           ?>
-        <script language="JavaScript">
-          <!--
-          //	document.blog.post_content.focus();
-          //-->
-        </script>
       </td>
     </tr>
   </table>
-    <?php echo $tablebottom ?>
+    <?= $tablebottom ?>
 </form>
