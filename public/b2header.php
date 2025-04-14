3e7a5028a60492b4de40aecd1d28b6edd4ea688b
Chuck Adams <cja987@gmail.com>
2025-04-14 16:42:58 -0600

    refactor: fix all warnings in b2header.php

--- public/b2header.php
+++ public/b2header.php
@@ -1,10 +1,12 @@
 <?php
 
+/** @noinspection DuplicatedCode */
+
 require_once("b2config.php");
-require_once($b2inc . "/b2template.functions.php");
-require_once($b2inc . "/b2verifauth.php");
-require_once($b2inc . "/b2vars.php");
-require_once($b2inc . "/b2functions.php");
+require_once(__DIR__ . '/' . $b2inc . "/b2template.functions.php");
+require_once(__DIR__ . '/' . $b2inc . "/b2verifauth.php");
+require_once(__DIR__ . '/' . $b2inc . "/b2vars.php");
+require_once(__DIR__ . '/' . $b2inc . "/b2functions.php");
 
 if (!isset($blogID)) {
     $blog_ID = 1;
@@ -34,29 +36,33 @@ if (($is_macIE) || ($is_lynx)) {
     $use_quicktags = 0;
 }
 
-$b2varstoreset = ['profile', 'standalone', 'redirect', 'redirect_url', 'a', 'popuptitle', 'popupurl', 'text'];
-for ($i = 0; $i < count($b2varstoreset); $i += 1) {
-    $b2var = $b2varstoreset[$i];
-    if (!isset($$b2var)) {
-        if (empty($_POST["$b2var"])) {
-            if (empty($_GET["$b2var"])) {
-                $$b2var = '';
-            } else {
-                $$b2var = $_GET["$b2var"];
-            }
-        } else {
-            $$b2var = $_POST["$b2var"];
-        }
-    }
-}
+$b2varstoreset = [
+    'profile',
+    'standalone',
+    'redirect',
+    'redirect_url',
+    'a',
+    'popuptitle',
+    'popupurl',
+    'text',
+];
 
-if ($standalone == 0) {
+$profile = $_REQUEST['profile'] ?? '';
+$standalone = $_REQUEST['standalone'] ?? 0;
+$redirect = $_REQUEST['redirect'] ?? 0;
+$redirect_url = $_REQUEST['redirect_url'] ?? '';
+$a = $_REQUEST['a'] ?? '';
+$popuptitle = $_REQUEST['popuptitle'] ?? '';
+$popupurl = $_REQUEST['popupurl'] ?? '';
+$text = $_REQUEST['text'] ?? '';
 
+if (!$standalone) {
 ?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
 <html>
 <head>
-  <title>b2 > <?php echo $title; ?></title>
-  <link rel="stylesheet" href="<?php echo $b2inc; ?>/b2.css" type="text/css">
+  <title>b2 > <?= $title ?></title>
+  <link rel="stylesheet" href="<?= $b2inc ?>/b2.css" type="text/css">
+  <!--suppress CssUnusedSymbol -->
   <style type="text/css">
     <!--
     <?php
@@ -68,8 +74,8 @@ if ($standalone == 0) {
       border-color: #999999;
       border-top-width: 1px;
       border-bottom-width: 1px;
-      border-left-width: 0px;
-      border-right-width: 0px;
+      border-left-width: 0;
+      border-right-width: 0;
       border-style: dashed;
     }
 
@@ -84,11 +90,11 @@ if ($standalone == 0) {
 
     .checkbox {
     <?php
-    if ((preg_match("/MSIE/",$HTTP_USER_AGENT)) && (!preg_match("/Mac/",$HTTP_USER_AGENT))) {
+    if ((str_contains($HTTP_USER_AGENT, "MSIE")) && (!str_contains($HTTP_USER_AGENT, "Mac"))) {
     ?> background-color: #ffffff;
-      border-width: 0px;
-      padding: 0px;
-      margin: 0px;
+      border-width: 0;
+      padding: 0;
+      margin: 0;
     }
 
     <?php
@@ -97,47 +103,31 @@ if ($standalone == 0) {
     ?>
     -->
   </style>
-    <?php
-    if ($redirect == 1) {
-        ?>
-      <script language="javascript">
-        <!--
-        function redirect() {
-          window.location = "<?php echo $redirect_url; ?>"
-        }
-
-        setTimeout("redirect();", 600)
-        //-->
+    <?php if ($redirect) { ?>
+      <script>
+        setTimeout(() => window.location.assign("<?= $redirect_url ?>"), 600)
       </script>
-        <?php
-    }
-    ?>
+    <?php } ?>
+  <!--suppress JSUnresolvedReference, JSDeprecatedSymbols -->
   <script language="javascript">
-    <!--
-    hiding
-    from
-    old
-    terrible
-    browsers
-
     function profile(userID) {
       window.open("b2profile.php?action=viewprofile&user=" + userID, "Profile", "width=500, height=450, location=0, menubar=0, resizable=0, scrollbars=1, status=1, titlebar=0, toolbar=0, screenX=60, left=60, screenY=60, top=60")
     }
 
     function preview(form) {
-      var preview_date = "<?php echo date("Y-m-d H:i:s"); ?>"
-      var preview_userid = "<?php echo $user_ID ?>"
-      var preview_title = form.post_title.value
-      var preview_category = form.post_category.value
-      var preview_content = form.content.value
-      var preview_autobr = form.post_autobr.value
+      let preview_date = "<?= date("Y-m-d H:i:s") ?>"
+      let preview_userid = "<?= $user_ID ?>"
+      let preview_title = form.post_title.value
+      let preview_category = form.post_category.value
+      let preview_content = form.content.value
+      let preview_autobr = form.post_autobr.value
       preview_date = escape(preview_date)
       preview_userid = escape(preview_userid)
       preview_title = escape(preview_title)
       preview_category = escape(preview_category)
       preview_content = escape(preview_content)
       preview_autobr = escape(preview_autobr)
-      window.open("<?php echo "$siteurl/$blogfilename" ?>?preview=1&preview_date=" + preview_date + "&preview_userid=" + preview_userid + "&preview_title=" + preview_title + "&preview_category=" + preview_category + "&preview_content=" + preview_content + "&preview_autobr=" + preview_autobr, "Preview", "location=0,menubar=1,resizable=1,scrollbars=yes,status=1,toolbar=0")
+      window.open("<?= "$siteurl/$blogfilename" ?>?preview=1&preview_date=" + preview_date + "&preview_userid=" + preview_userid + "&preview_title=" + preview_title + "&preview_category=" + preview_category + "&preview_content=" + preview_content + "&preview_autobr=" + preview_autobr, "Preview", "location=0,menubar=1,resizable=1,scrollbars=yes,status=1,toolbar=0")
     }
 
     function launchupload() {
@@ -147,7 +137,7 @@ if ($standalone == 0) {
     //  End -->
   </script>
 </head>
-<body bgcolor="#ffffff" text="#000000" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">
+<body bgcolor="#ffffff" text="#000000">
 
 <table width="100%" cellpadding="0" cellspacing="0" align="center">
     <?php
@@ -156,7 +146,7 @@ if ($standalone == 0) {
     }
     if ($profile === 0) {
     ?>
-  <tr height="60">
+  <tr>
     <td valign="top">
         <?php include($b2inc . "/b2menutop.php") ?>
     </td>
@@ -166,8 +156,7 @@ if ($standalone == 0) {
       }
       ?>
     <td valign="top">
+      <!--suppress CheckImageSize -->
       <img src="b2-img/blank.gif" border="0" width="35" height="24"/>
       <div class="panelbody">
-<?php
-}
-?>
+<?php } ?>
